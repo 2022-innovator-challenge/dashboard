a2a4173f93ec73955fc48c9e46ac42f1643a7c72
Marika Marszalkowski <marika.marszalkowski@sap.com>
2022-08-25 14:34:43 +0200

    Add backend service for k8s API

---
+++ pages/api/configmaps.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Agent } from 'node:https';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Record<string, string>>
+) {
+  const mountDir = process.env.SERVICE_ACCOUNT_MOUNT_DIR || '.';
+  const token = await readFile(join(mountDir, 'token'));
+  const ca = await readFile(join(mountDir, 'ca.crt'));
+
+  const opt = {
+    method: req.method,
+    headers: { Authorization: `Bearer ${token}` },
+    agent: new Agent({ ca })
+  };
+
+  if (req.method === 'PATCH' || req.method === 'GET') {
+    const response = await fetch(
+      'https://api.c2aecf0.kyma.ondemand.com/api/v1/namespaces/team2001/configmaps/downloadstats-packages-cm',
+      opt
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } else {
+    res.status(405);
+  }
+}
